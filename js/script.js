39c83138698fdc04e306b598260d85e232f4c3bf
unknown <p6579113@gmail.com>
2024-08-14 14:22:39 +0530

    done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-//toggle
-let menubtn = document.getElementById('menubtn')
-mentbtn.addEventListener('click',function(e){
-    document.querySelector('body').classList.toggle('moile-nav-active');
-    this.classList.toggle('bi-x-lg')
+$(function() {
+    $('.selector').animatedHeadline({
+        animationType: 'type'
+    });
 })
